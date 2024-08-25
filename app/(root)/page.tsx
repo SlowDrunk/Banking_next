4df7a47281aeea379823bf3927314aa9dbf48202
liujian <2359944605@qq.com>
2024-08-25 21:52:04 +0800

    feat: "新增公共头部组件"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+// import { useTranslation } from 'next-i18next'
 
 export default function Home() {
+    const loggedIn = { firstName: "Admin" }
+    // const { t } = useTranslation()
     return (
-        <div>Home</div>
+        <section className='home'>
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstName || 'Guest'} subtext="Access and manage your account and transactions efficiently." />
+                    <TotalBalanceBox
+                        accounts={[]} totalBanks={1} totalCurrentBalance={1250.13}></TotalBalanceBox>
+                </header>
+            </div>
+        </section>
     )
 }
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
+
+export default function TotalBalanceNox({ accounts, totalBanks, totalCurrentBalance }: TotalBalanceBoxProps) {
+
+  return (
+    <section className='total-balance'>
+      <div className='total-balance-chart'>
+        <DoughnutChart />
+      </div>
+      <div className='flex flex-col gap-6'>
+        <h2 className='header-2'>
+          银行账户数量：{totalBanks}
+        </h2>
+        <div className='flex flex-col gap-2'>
+          <p className="total-balance-label">
+            总余额
+          </p>
+          <p className='total-balance-amount flex-center gap-2'>
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
