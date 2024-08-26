49a900333f17f35e49ebb0d3b8be9c1ff5625b5c
liujian <2359944605@qq.com>
2024-08-26 22:06:38 +0800

    feat: "添加右侧导航"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 // import { useTranslation } from 'next-i18next'
 
 export default function Home() {
-    const loggedIn = { firstName: "Admin" }
+    const loggedIn = { firstName: "Admin", lastName: 'JSM',email:'admin@jsm.com' }
     // const { t } = useTranslation()
     return (
         <section className='home'>
@@ -15,7 +16,9 @@ export default function Home() {
                     <TotalBalanceBox
                         accounts={[]} totalBanks={1} totalCurrentBalance={1250.13}></TotalBalanceBox>
                 </header>
+                RENCENT CONTENT
             </div>
+            <RightSidebar user={loggedIn} transactions={[]} banks={[]}></RightSidebar>
         </section>
     )
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -15,7 +15,7 @@ export default function SideBar({ user }: SiderbarProps) {
             <nav className='flex flex-col gap-4'>
                 <Link href="/" className='mb-12 cursor-pointer items-center gap-2'>
                     <Image src={'/icons/logo.svg'} width={34} height={34} alt='Horizon logo' className='size-[24px] max-xl:size-14' />
-                    <h1 className='sidebar-logo'>Horizon</h1>
+                    <h3 className='sidebar-logo'>Horizon</h3>
                 </Link>
                 {sidebarLinks.map((item) => {
                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
